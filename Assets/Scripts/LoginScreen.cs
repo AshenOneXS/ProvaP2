5f6e348856807091c2669d436fef64ceecf54342
David <dd.gomes202@gmail.com>
2024-11-29 21:47:51 -0300

    Envio da Prova

---
+++ Assets/Scripts/LoginScreen.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+
+
+
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    public TMP_InputField txtUsuario;
+
+    private void Start()
+    {
+
+ 
+
+
+
+    }
+}
+
+
+
